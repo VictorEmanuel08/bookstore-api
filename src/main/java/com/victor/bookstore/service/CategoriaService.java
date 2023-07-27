1da1a94cc3c05bba17551ed7aa15fff95d95bc2c
VictorEmanuel08 <victorem3181@gmail.com>
2023-07-27 18:11:24 -0300

    UPDATE para categoria

--- src/main/java/com/victor/bookstore/service/CategoriaService.java
+++ src/main/java/com/victor/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victor.bookstore.domain.Categoria;
+import com.victor.bookstore.dtos.CategoriaDTO;
 import com.victor.bookstore.repositories.CategoriaRepository;
 import com.victor.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 	}
 }
