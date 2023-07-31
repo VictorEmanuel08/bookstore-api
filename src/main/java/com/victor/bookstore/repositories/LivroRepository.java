a3bd665999d582477c11c734a97b024f11b738e6
VictorEmanuel08 <victorem3181@gmail.com>
2023-07-31 17:54:14 -0300

    Listando todos os livros por categoria

--- src/main/java/com/victor/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/victor/bookstore/repositories/LivroRepository.java
@@ -1,18 +1,18 @@
 package com.victor.bookstore.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 //import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.victor.bookstore.domain.Livro;
 
-//informar para o spring que estamos criando
-//uma interface reposirory
-
-//ele espera dois argumentos: 
-//1 - A classe implementada (a camadad e comuncação)
-//2 - O tipo primitivo da variavel utilizada como chave primária (identificadora da classe)
 //@Repository
-public interface LivroRepository extends JpaRepository<Livro, Integer>{
-	
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+	@Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY  titulo")
+	List<Livro> findAllByCategoria(@Param(value = "id_cat") Integer id_cat);
 
 }
