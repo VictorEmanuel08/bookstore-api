f01ab66bda6ec6fe3559baf78815d55bf2173033
VictorEmanuel08 <victorem3181@gmail.com>
2023-08-02 17:14:03 -0300

    POST para livro

--- src/main/java/com/victor/bookstore/service/LivroService.java
+++ src/main/java/com/victor/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.victor.bookstore.domain.Categoria;
 import com.victor.bookstore.domain.Livro;
 import com.victor.bookstore.repositories.LivroRepository;
 import com.victor.bookstore.service.exceptions.ObjectNotFoundException;
@@ -41,5 +42,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
